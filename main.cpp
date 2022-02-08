4a95940de80c7ac09020ba9abb6c0ac6c51680c7
BCU20132506 <connor.hadley@mail.bcu.ac.uk>
2022-02-08 13:11:14 +0000

    Load image fork

--- main.cpp
+++ main.cpp
@@ -11,12 +11,85 @@ const int width  = 800;
 const int height = 800;
 
 // Write the line method here
-// line (x0, y0, x1, y1, image, colour)
 
+//void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+//    for (float t = 0; t < 1; t = t + 0.001) {
+//
+//        int x = x0 + (x1 - x0) * t;
+//        int y = y0 + (y1 - y0) * t;
+//
+//        image.set(x, y, color);
+//    }
+//}
+
+//void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+//    for (float x=x0; x <=x; x++){
+//
+//        int t = (x - x0) / (x1 - x0);
+//        int y = y0 * (1 - t) + (y1 * t);
+//
+//            image.set(x, y, color);
+//    }
+//}
+
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color) {
+
+    bool steep = false;
+
+    if (abs(x0 - x1) < abs(y0 - y1)) {
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+        steep = true;
+    }
+
+    if (x0 > x1) {
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    for (float x = x0; x <= x1; x++)
+    {
+        float t = (x - x0) / (x1 - x0);
+        float y = y0 * (1 - t) + y1 * t;
+
+        if (steep == true) {
+            image.set(y, x, color);
+        }
+        else {
+            image.set(x, y, color);
+        }
+    }
+}
+ 
 int main(int argc, char** argv) {
-    
+
     TGAImage image(width, height, TGAImage::RGB);
 
+    //line(200, 200, 600, 600, image, red);
+
+    if (2 == argc)
+    {
+        model = new Model(argv[1]);
+    }
+
+    else {
+        model = new Model("cc.obj");
+    }
+
+    for (int i = 0; i < model->nfaces(); i++) {
+
+        std::vector<int> face = model->face(i);
+        for (int j = 0; j < 3; j++) {
+            Vec3f v0 = model->vert(face[j]);
+            Vec3f v1 = model->vert(face[(j + 1) % 3]);
+            int x0 = (v0.x + 1.) * width / 2.;
+            int y0 = (v0.y + 1.) * height / 2.;
+            int x1 = (v1.x + 1.) * width / 2.;
+            int y1 = (v1.y + 1.) * height / 2.;
+            line(x0, y0, x1, y1, image, white);
+
+        }
+    }
     image.flip_vertically(); // we want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     delete model;
